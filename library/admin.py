eb69fcc6d1d606dfd605b7494cbb386c99c234a7
Jolita <joliabukauskaite@gmail.com>
2026-04-16 18:53:41 +0300

    Aprasytas ir registruotas like modeliai

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
 
-from .models import Author
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'isbn']
+    inlines = [BookInstanceInLine]
+
+
 admin.site.register(Author)
+
+admin.site.register(Genre)
+
+admin.site.register(Book, BookAdmin)
+
+admin.site.register(BookInstance)
+
--- library/models.py
+++ library/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,38 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.first_name}'
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    summary = models.TextField()
+    isbn = models.IntegerField()
+    author = models.ForeignKey(to='Author',
+                               on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    genre = models.ManyToManyField(to='Genre')
+    def __str__(self):
+        return self.title
+
+class BookInstance(models.Model):
+    book = models.ForeignKey(to='Book', on_delete=models.CASCADE)
+    due_back = models.DateField()
+    reader = models.ForeignKey(to=User,
+                               on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    LOAN_STATUS = (
+        ('d', 'Administered'),
+        ('t', 'Taken'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(choices=LOAN_STATUS, default='d')
+
+    def __str__(self):
+        return f'{self.book}'
